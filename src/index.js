b29db93a6fc8315622e3147ce8b4c0a13ccc8464
xDAnkit <xDAnkitjain@gmail.com>
2023-01-03 14:38:19 +0530

    Added react-native-web support

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import { AppRegistry } from "react-native";
+import App from "./App";
+
+AppRegistry.registerComponent("AwesomeTSProject", () => App);
+AppRegistry.runApplication("AwesomeTSProject", {
+  rootTag: document.getElementById("root"),
+});
